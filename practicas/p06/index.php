56eb8edd1ff4a7d2d10469022dfa7eb1b734d20b
John <jonathan.vergaran@alumno.buap.mx>
2025-02-06 18:48:34 -0600

    Se realizo el ejercicio2 en index.php y su función en funciones.php

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -32,5 +32,12 @@
             echo $_POST["email"];
         }
     ?>
+
+    <h2>Ejercicio 2</h2>
+    <p>Crea un programa para la generación repetitiva de 3 números aleatorios hasta obtener una
+    secuencia compuesta por: impar, par, impar</p>
+    <?php
+        generar_secuencia();
+    ?>
 </body>
 </html>
--- practicas/p06/src/funciones.php
+++ practicas/p06/src/funciones.php
@@ -12,4 +12,46 @@ function es_multiplo7y5($num)
     }
 }
 
+function generar_secuencia()
+{
+    $matriz = array();
+    $num_generados = 0;
+    $iteraciones = 0;
+
+    // Generar una matriz de 4x3
+    for ($i = 0; $i < 4; $i++) {
+        for ($j = 0; $j < 3; $j++) {
+            $matriz[$i][$j] = rand(1, 1000);
+            $num_generados++;
+        }
+    }
+
+    // Imprimir la matriz generada
+    echo '<h3>Matriz generada:</h3>';
+    for ($i = 0; $i < 4; $i++) {
+        for ($j = 0; $j < 3; $j++) {
+            echo $matriz[$i][$j] . ' ';
+        }
+        echo '<br>';
+    }
+
+    // Buscar la secuencia específica
+    for ($i = 0; $i < 4; $i++) {
+        for ($j = 2; $j < 3; $j++) {
+            $iteraciones++;
+            if ($matriz[$i][$j-2] % 2 != 0 && $matriz[$i][$j-1] % 2 == 0 && $matriz[$i][$j] % 2 != 0) {
+                echo '<h3>R= La secuencia es: ' . $matriz[$i][$j-2] . ', ' . $matriz[$i][$j-1] . ', ' . $matriz[$i][$j] . ' en la fila ' . $i . '</h3>';
+                echo '<h3>Número de iteraciones: ' . $iteraciones . '</h3>';
+                echo '<h3>Cantidad de números generados: ' . $num_generados . '</h3>';
+                return;
+            }
+        }
+    }
+
+    echo '<h3>No se encontró la secuencia específica.</h3>';
+    echo '<h3>Número de iteraciones: ' . $iteraciones . '</h3>';
+    echo '<h3>Cantidad de números generados: ' . $num_generados . '</h3>';
+}
+?>
+
 ?>
