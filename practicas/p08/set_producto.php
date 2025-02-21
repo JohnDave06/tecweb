32b5bb25157268ecef1555eb56e9b188558cffbc
John <jonathan.vergaran@alumno.buap.mx>
2025-02-20 19:52:01 -0600

    Se creo la carpeta p08 y dentro de ella se creo la carpeta ejercicio1. La cual contiene el ejercicio del concurso

---
+++ practicas/p08/set_producto.php
@@ -0,0 +1,32 @@
+<?php
+$nombre = 'nombre_producto';
+$marca  = 'marca_producto';
+$modelo = 'modelo_producto';
+$precio = 1.0;
+$detalles = 'detalles_producto';
+$unidades = 1;
+$imagen   = 'img/imagen.png';
+
+/** SE CREA EL OBJETO DE CONEXION */
+@$link = new mysqli('localhost', 'root', 'contraseña_6', 'marketzone');	
+
+/** comprobar la conexión */
+if ($link->connect_errno) 
+{
+    die('Falló la conexión: '.$link->connect_error.'<br/>');
+    /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+}
+
+/** Crear una tabla que no devuelve un conjunto de resultados */
+$sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}', 0)";
+if ( $link->query($sql) ) 
+{
+    echo 'Producto insertado con ID: '.$link->insert_id;
+}
+else
+{
+	echo 'El Producto no pudo ser insertado =(';
+}
+
+$link->close();
+?>
