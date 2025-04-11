fd77ca65021e37d34c033f921575cccbe8ff9243
John <jonathan.vergaran@alumno.buap.mx>
2025-04-11 13:32:46 -0600

    Se actualizó el archivo index.php

--- practicas/p17/index.php
+++ practicas/p17/index.php
@@ -22,6 +22,21 @@
         return $response;
     });
 
+    /*  
+    // esta version es del video de SLim v3 por eso no funciona el siguiente código
+    $app->get('/hola/{nombre}', function ( $request, $response , $args ) 
+    {}
+        $response->write("Hola, " . $args["nombre]. "!");
+        return $response;
+    });
+ */
+
+     // Esta es la versión correcta para Slim 4 lectura de url
+    $app->get('/hola/{nombre}', function (Request $request, Response $response, $args) {
+        $response->getBody()->write("Hola, " . $args["nombre"] . "!");
+        return $response;
+    });
+
      $app->run(); // ejecutar el servidor web Slim 4
 
 ?>
